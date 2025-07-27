f6b911ffaf2f13ea645c3eba1fd0d020222c28a6
Daniel Naiff <danielnaiff344@gmail.com>
2025-07-27 02:31:09 -0300

    feat: adicionar transfer entity

---
+++ src/main/java/com/danielnaiff/backend/entity/Transfer.java
@@ -0,0 +1,34 @@
+package com.danielnaiff.backend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_transfer")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transfer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_sender_id")
+    private User payer;
+
+    @ManyToOne
+    @JoinColumn(name = "user_receiver_id")
+    private User payee;
+
+    @Column(name = "value")
+    private BigDecimal value;
+}
