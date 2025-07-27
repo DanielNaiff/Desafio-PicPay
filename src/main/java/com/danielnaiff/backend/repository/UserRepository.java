bf73be0c384f3fe39b17bc5a919396eb7282edf2
Daniel Naiff <danielnaiff344@gmail.com>
2025-07-27 01:41:35 -0300

    feat: adicionar novos metodos ao user repository

--- src/main/java/com/danielnaiff/backend/repository/UserRepository.java
+++ src/main/java/com/danielnaiff/backend/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.danielnaiff.backend.repository;
 import com.danielnaiff.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> existsByEmail();
+    Optional<User> existsByDocument();
 }
